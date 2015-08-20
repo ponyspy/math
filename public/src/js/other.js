a120c6d16456bde8ea8d6dbaf15079616c5fa94c
ponyspy <desade4me@gmail.com>
2015-08-20 18:11:11 +0300

    scroll

--- public/src/js/other.js
+++ public/src/js/other.js
@@ -5,24 +5,35 @@ $(document).ready(function() {
 		category: window.location.hash === '' ? 'all' : window.location.hash.replace('#','')
 	};
 
-	$(window).on('scroll', function(event) {
-		$(window).scrollTop() == $(document).height() - $(window).height()
-			&& $('.other_item').length < 60
-			&& $('.other_item').slice(0,3).clone().appendTo('.studys_other');
-	});
+	function scrollLoader(event) {
+		if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+			$.ajax({url: '/other', method: 'POST', data: {context: context}, async: false }).done(function(data) {
+				if (data != '') {
+					$('.studys_other').append(data).find('.social-likes').socialLikes();
+					context.skip += 10;
+				}
+				else $(window).off('scroll');
+			});
+		}
+	}
 
-	$(window).on('load hashchange', function(event) {
-		context.skip = 0;
-		context.limit = 20;
-		context.category = window.location.hash === '' ? 'all' : window.location.hash.replace('#','');
+	$(window)
+		.on('scroll', scrollLoader)
+		.on('load hashchange', function(event) {
+			context.skip = 0;
+			context.limit = 20;
+			context.category = window.location.hash === '' ? 'all' : window.location.hash.replace('#','');
 
-		var $category_item = $('.' + context.category);
-		var index = $category_item.index('.category_item');
+			var $category_item = $('.' + context.category);
+			var index = $category_item.index('.category_item');
 
-		$('.category_item').removeClass('current').eq(index).addClass('current');
-		$.post('/other', {context: context}).done(function(data) {
-			$('.studys_other').empty().append(data);
-			$('.social-likes').socialLikes();
+			$('.category_item').removeClass('current').eq(index).addClass('current');
+			$.ajax({url: '/other', method: 'POST', data: {context: context}, async: false }).done(function(data) {
+				var elems = $(data);
+				context.skip = elems.length;
+
+				$('.studys_other').empty().append(elems).find('.social-likes').socialLikes();
+				$(window).on('scroll', scrollLoader);
+			});
 		});
-	});
 });
