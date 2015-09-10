63fb99fb5821487b33ce83956e336c428e71fd1a
ponyspy <desade4me@gmail.com>
2015-09-10 12:22:55 +0300

    text

--- public/src/js/write.js
+++ public/src/js/write.js
@@ -3,7 +3,7 @@ var validate = {
 		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return re.test(email);
 	}
-}
+};
 
 $(document).ready(function() {
 	$('.plane_block').on('click', function() {
