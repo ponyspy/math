cb95a4be0333d99816a6b32e61034f0744f10ab8
ponyspy <desade4me@gmail.com>
2015-09-25 13:41:55 +0300

    animate

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 	$('.round_big').plaxify({'xRange': 140, 'yRange': 140, 'invert': true, 'useTransform': false});
+	$('.rounds_group').plaxify({'xRange': 40, 'yRange': 0, 'invert': false, 'useTransform': false});
 	$('.blocks_group').plaxify({'xRange': 40, 'yRange': 0, 'invert': true, 'useTransform': false});
 	$.plax.enable();
+
+	$('.round_small').on('click', function() {
+		$(this).off().animate({
+			top: '+=110px',
+			left: '-=320px'
+		}, 800, function() {
+			$(this).hide();
+		});
+	});
 });
