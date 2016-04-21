8f53b51634adbf119303ca1380dad3d07d7c1aa1
ponyspy <desade4me@gmail.com>
2016-04-21 23:47:54 +0300

    books

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,10 @@ var path = require('path');
 var __appdir = path.dirname(require.main.filename);
 
 exports.index = function(req, res) {
-	var books = JSON.parse(fs.readFileSync(__appdir + '/views/main/books.json', 'utf8'));
-	console.log(books)
+	try {
+		var books = JSON.parse(fs.readFileSync(__appdir + '/views/main/books.json'));
+	} catch(e) {
+		var books = [];
+	}
 	res.render('main', {books: books});
 }
